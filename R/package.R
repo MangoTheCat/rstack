d9638f12d13b5f2a7571e32d9c6d72a5aad13c97
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-01 21:33:14 +0200

    Fix up mason mistakes

--- R/package.R
+++ R/package.R
@@ -1,9 +1,9 @@
 
 #' Stack Data Types as an 'R6' Class
 #'
-#' An extremely simple stack data type, implemented with 'R6' classes. The size
-  of the stack increases as needed, and the amortized time complexity is O(1).
-  The stack may contain arbitrary objects.
+#' An extremely simple stack data type, implemented with 'R6' classes. The
+#' size of the stack increases as needed, and the amortized time
+#' complexity is O(1). The stack may contain arbitrary objects.
 #'
 #' @docType package
 #' @name stack
