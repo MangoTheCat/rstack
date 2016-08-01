966df56b32eafee48cd6426b2737051c6a134dbd
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-02 00:04:06 +0200

    Minor DESCRIPTION, README, docs cleanup

--- R/package.R
+++ R/package.R
@@ -1,14 +1,4 @@
 
-#' Stack Data Types as an 'R6' Class
-#'
-#' An extremely simple stack data type, implemented with 'R6' classes. The
-#' size of the stack increases as needed, and the amortized time
-#' complexity is O(1). The stack may contain arbitrary objects.
-#'
-#' @docType package
-#' @name stack
-NULL
-
 #' A stack data type, implemented as an R6 class
 #'
 #' Methods: \itemize{
