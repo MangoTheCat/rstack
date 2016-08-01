240a62acc4c41423bce7ac70b546827579151747
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-02 00:00:28 +0200

    Manual page

--- R/package.R
+++ R/package.R
@@ -9,8 +9,34 @@
 #' @name stack
 NULL
 
+#' A stack data type, implemented as an R6 class
+#'
+#' Methods: \itemize{
+#' \item \code{push(elem)} Puts an R object on the top of the stack.
+#'   This operation might trigger the allocation of memory, if the
+#'   currently allocated memory cannot hold the new element. Still, the
+#'   amortized time complexity of the stack is constant.
+#'   The stack may contain arbitrary R objects.
+#' \item \code{pop()} Removes and returns the top element of the stack.
+#'   It throws an error if the stack is empty.
+#' \item \code{peek()} Returns the top element of the stack. (Without
+#'   removing it.) It throws an error if the stack is empty.
+#' \item \code{size()} Returns the number of elements in the stack.
+#' \item \code{is_empty()} Returns wheather the stack is empty.
+#' }
+#'
 #' @export
 #' @importFrom R6 R6Class
+#' @examples
+#' S <- stack$new()
+#' S$push(1L)
+#' S$peek()
+#' S$pop()
+#' S$size()
+#'
+#' S$push(NULL)
+#' S$push(iris)
+#' colnames(S$peek())
 
 stack <- R6Class(
   "stack",
